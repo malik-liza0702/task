5123245387771e3ad99c056d2202b18a1b7655cf
malik-liza <malik.liza0702@gmail.com>
2025-01-17 16:11:26 +0530

    first

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import left from "../../assets/arrow-left.png"
+import toggle from "../../assets/Toggle.png"
+import frame from "../../assets/Frame 1.png"
+function Dashboard() {
+  return (
+    <>
+    <div className="flex flex-col">
+        <div className="p-2 flex justify-between items-center">
+            <div className="flex">
+                <img src={left}></img>
+                <p>Name of the file</p>
+            </div>
+            <div className="flex">
+                <img src={toggle}></img>
+                <img src={frame}></img>
+            </div>
+
+        </div>
+        <div>
+ffkekekgvenkgekbvebkebfkebkebke
+        </div>
+
+
+    </div>
+    </>
+  )
+}
+
+export default Dashboard
